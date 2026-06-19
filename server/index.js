b971e362fea83646ae28a2436b8d07bd1c47b961
srraghunath <srraghu2005@gmail.com>
2026-06-19 11:15:26 +0530

    Fix Express 5 wildcard path parsing using regex

--- server/index.js
+++ server/index.js
@@ -63,7 +63,7 @@ app.get('/api/health', (req, res) => res.json({ status: 'ok' }));
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
